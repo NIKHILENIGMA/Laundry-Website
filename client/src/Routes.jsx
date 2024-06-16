4b2ac006022789bd39f5c084337e6a10728d03b0
nikhilenigma <nickharmalkar18@gmail.com>
2024-06-17 00:22:21 +0530

    Rename the file as Routes.jsx

---
+++ client/src/Routes.jsx
@@ -0,0 +1,30 @@
+import React, { Suspense } from 'react'
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom"
+import MainLayout from "./pages"
+
+const Home = React.lazy(() => import("./pages/Home"))
+const NotFound = React.lazy(() => import("./pages/NotFound")) 
+const Order = React.lazy(() => import("./pages/Order")) 
+const Payment = React.lazy(() => import("./pages/Payment")) 
+const Services = React.lazy(() => import("./pages/Services")) 
+
+function ProjectRoutes() {
+  return (
+    <Suspense fallback={<>Loading...</>}>
+        <Router>
+            <Routes>
+                <Route path='/' element={<MainLayout />}/>
+                <Route path='' element={<Home />}/>
+                <Route path='order' element={<Order />}/>
+                <Route path='payment' element={<Payment />}/>
+                <Route path='services' element={<Services />}/>
+
+                <Route path='*' element={<NotFound/>} />
+            </Routes>  
+        </Router>
+    </Suspense>
+  )
+}
+
+export default ProjectRoutes
+
