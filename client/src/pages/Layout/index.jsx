3f91d9c0b92e6aa9b64d6f00952cb6e8e3ea137f
nikhilenigma <nickharmalkar18@gmail.com>
2024-06-17 12:59:14 +0530

    Added a Layout to which render the main contain inside the Header and Footer component

---
+++ client/src/pages/Layout/index.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import { Header, Footer } from "../../components"
+import { Outlet } from 'react-router-dom'
+
+function MainLayout() {
+  return (
+    <div>
+        <Header />
+        <main>
+            <Outlet />
+        </main>
+        <Footer />
+    </div>
+  )
+}
+
+export default MainLayout
