e937c2085e01c6c6261c079e7df424051c371240
nikhilenigma <nickharmalkar18@gmail.com>
2024-06-17 13:00:34 +0530

    Bad Request Page (Error 404 ) added for all the request which do not have any proper path describe

---
+++ client/src/pages/NotFound/index.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function NotFound() {
+  return (
+    <div className="flex items-center justify-center w-full text-3xl font-bold h-[100vh]">
+      Error 404
+    </div>
+  );
+}
+
+export default NotFound;
