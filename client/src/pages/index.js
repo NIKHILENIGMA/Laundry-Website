3fb70c38227148fc5c7f9468f2c24b95c04512d2
nikhilenigma <nickharmalkar18@gmail.com>
2024-06-18 11:14:22 +0530

    Added All the pages to index.js for simple export

---
+++ client/src/pages/index.js
@@ -0,0 +1,13 @@
+import MainLayout from "./Layout";
+import Home from "./Home";
+import NotFound from "./NotFound";
+import Order from "./Order";
+import Payment from "./Payment";
+import Services from "./Services";
+import PriceList from "./PriceList";
+import ServiceDetails from "./ServiceDetails";
+
+
+
+
+export { MainLayout, Home, NotFound, Order, Payment, Services, PriceList, ServiceDetails };
